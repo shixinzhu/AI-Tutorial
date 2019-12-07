160a91a7724bdda33abeb8d48f61acef444b6525
Ningfeng Yang <nf.yang@lifevar.com>
2019-12-07 16:51:27 +0900

    Lesson1宿題

---
+++ homework/lesson01/yang/kuku.py
@@ -0,0 +1,13 @@
+from  multipulation import multiple
+for n in range(9):
+    for n2 in range(9):
+        multiple(n+1, n2+1)
+    print()
+
+"""
+import multipulation as m
+for n in range(9):
+    for n2 in range(9):
+        m.multiple(n+1, n2+1)
+    print()
+"""
---
+++ homework/lesson01/yang/multipulation.py
@@ -0,0 +1,2 @@
+def multiple(n, m):
+    print("{} * {} = {}".format(n, m, n*m), end=" ")
