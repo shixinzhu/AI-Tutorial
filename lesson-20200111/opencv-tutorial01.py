2a7f69e592e97219b35d4ff44427d7bce3ba907c
sincava-mac <sincava@gmail.com>
2020-01-05 15:34:45 +0900

    add images

--- lesson-20200111/opencv-tutorial01.py
+++ lesson-20200111/opencv-tutorial01.py
@@ -3,6 +3,7 @@ import cv2
 src = cv2.imread('senba-takako-profile.jpg')
 cv2.namedWindow("imput image", cv2.WINDOW_AUTOSIZE)
 cv2.imshow("imput image", src)
+
 cv2.waitKey(0)
 cv2.destroyAllowWindows()
 
